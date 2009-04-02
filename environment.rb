3241d3712b08548a6be800519daa754e5f3a5569
Stephen Caudill <vox@exdolo.com>
2009-04-01 22:40:44 -0400

    Restructuring the application layout
    * models in lib rather than lib/models
    * specs (unit specs anyway) in spec/units rather than spec/models

--- environment.rb
+++ environment.rb
@@ -1,5 +1,5 @@
 require 'datamapper'
 DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/db/ding.sqlite3")
 
-Dir.glob(File.join(File.dirname(__FILE__),'lib','models','*')){ |f| require f }
+Dir.glob(File.join(File.dirname(__FILE__),'lib','*.rb')){ |f| require f }
 
