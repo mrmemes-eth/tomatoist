73965eb3b83fe650e2ae583e506a62012cd8aa46
Stephen Caudill <vox@exdolo.com>
2009-05-10 22:35:28 -0400

    Alphabetizing methods again...

--- lib/timer.rb
+++ lib/timer.rb
@@ -27,12 +27,17 @@ class Timer
     all(:order => [:created_at.desc], :limit => 8)
   end
 
+  def timer=(type)
+    self.duration = MAPPINGS[type][:duration]
+  end
+
+
   def created_at
     zone.local_to_utc(attribute_get(:created_at))
   end
 
-  def zone
-    TZInfo::Timezone.get(offset ? "Etc/GMT#{offset}" : 'UTC')
+  def created_today?
+    created_at.day == Time.now.utc.day
   end
 
   def display_time
@@ -45,26 +50,22 @@ class Timer
     offset == '0' ? time.gsub!(/(AM|PM)/,'\1 UTC') : time
   end
 
-  def created_today?
-    created_at.day == Time.now.utc.day
-  end
-
-  def timer=(type)
-    self.duration = MAPPINGS[type][:duration]
+  def expiry
+    created_at + duration
   end
 
   def name
     MAPPINGS.detect{|k,v| v[:duration] == duration}.last[:long_name]
   end
 
-  def expiry
-    created_at + duration
-  end
-
   def to_js
     a = expiry.to_a[0..5].reverse
     a[1] = a[1] - 1
     a
+  end
+
+  def zone
+    TZInfo::Timezone.get(offset ? "Etc/GMT#{offset}" : 'UTC')
   end
 
 end
