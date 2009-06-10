3d565a7889539ae2a725fe1b460b479a6b589c89
Stephen Caudill <vox@exdolo.com>
2009-06-09 21:31:43 -0400

    Keyboard shortcuts for timers (opt+s,opt+l,opt+p)

--- public/js/pomodoro.js
+++ public/js/pomodoro.js
@@ -1,6 +1,22 @@
 $.countdown.setDefaults({alwaysExpire: true, format: 'MS'})
 soundManager.url = 'swf/'
 
+var isCtrl = false;
+
+$(document).keyup(function(e){
+  if(e.which == 18) isOpt = false;
+});
+
+$(document).keydown(function(e){
+  // console.log(e.type,"keyCode:"+e.keyCode,"which:"+e.which,"charCode:"+String.fromCharCode(e.which));
+  if(e.which == 18) isOpt = true;
+  if(isOpt){
+    if($.inArray(e.keyCode, [80,83,76]) > -1){
+      $('#' + String.fromCharCode(e.which).toLowerCase()).submit();
+    }
+  }
+});
+
 function minToMil(minutes){
   return 1000*60*minutes;
 }
