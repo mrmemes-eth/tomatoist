409e1d004bd11bab5e750bde27c5aa6479c1d202
Stephen Caudill <vox@exdolo.com>
2009-08-13 21:28:14 -0400

    Move database configuration to its own file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 require 'datamapper'
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3:db/ding.sqlite3")
+require 'erb'
 
-Dir.glob(File.join(File.dirname(__FILE__),'..','lib','**','*.rb')){ |f| require f }
+config_dir = File.expand_path(File.dirname(__FILE__))
+database_file = File.read(File.join(config_dir,'database.yml'))
+database_hash = YAML.load(ERB.new(database_file).result)
+
+Dir.glob(File.join(config_dir,'..','lib','**','*.rb')) do |file|
+  require file
+end
+
+DataMapper.setup(:default, database_hash[Ding.environment])
 
