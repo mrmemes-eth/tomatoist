00fbb852ebbac08c86eb0ef5c393450bee0a73ee
Jon Larkowski and Stephen Caudill <dev+l4rk+voxdolo@hashrocket.com>
2009-09-25 12:10:15 -0400

    Modularize controller helpers

--- lib/ding.rb
+++ lib/ding.rb
@@ -4,32 +4,39 @@ require 'haml'
 
 require 'config/environment'
 
+module DingHelpers
+  def root_path
+    "/"
+  end
+  def session_path(session)
+    "/#{session.display_name}"
+  end
+  def session_reset_path(session)
+    "/#{session.display_name}/reset"
+  end
+  def session_timers_path(session)
+    "#{session_path(session)}/timers"
+  end
+  def timer
+    return nil unless @session
+    @timer ||= @session.last_timer
+  end
+  def next_timer
+    @next_timer ||= @session.next_timer
+  end
+  def timer_class(session,type)
+    case
+    when session.next_timer == type; 'next'
+    when session.last_timer.kind_of?(type); 'current'
+    end
+  end
+end
+
 class Ding < Sinatra::Default
   enable :raise_errors
 
   helpers do
-    def root_path
-      "/"
-    end
-    def session_path(session)
-      "/#{session.display_name}"
-    end
-    def session_reset_path(session)
-      "/#{session.display_name}/reset"
-    end
-    def session_timers_path(session)
-      "#{session_path(session)}/timers"
-    end
-    def timer
-      return nil unless @session
-      @timer ||= @session.last_timer
-    end
-    def next_timer
-      @next_timer ||= @session.next_timer
-    end
-    def new_timer_class(session,type)
-      session.next_timer == type ? 'next' : nil
-    end
+    include DingHelpers
   end
 
   get '/' do
