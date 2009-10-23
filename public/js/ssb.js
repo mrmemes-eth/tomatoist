8395cad71dacc86c8af5c10d5eaa9441e25dedbd
Stephen Caudill <vox@exdolo.com>
2009-10-22 21:50:44 -0400

    Provide growl and dock support for Prism

--- public/js/ssb.js
+++ public/js/ssb.js
@@ -9,18 +9,31 @@ $(function(){
     window.fluid.addDockMenuItem("Long Break", function(){
       $('#long').click();
     });
+  } else if(window.platform) {
+    /* START HACK: the following fixes some odd behavior in prism
+     * where it re-inserts menu items over and over again. this
+     * works around that undesirable bevhavior*/
+    window.platform.icon().menu.removeAllMenuItems();
+    // END HACK
+
+    window.platform.icon().menu.addMenuItem('pomo',"Pomdoro", function(){
+      $('#pomodoro').click();
+    });
+    window.platform.icon().menu.addMenuItem('short',"Short Break", function(){
+      $('#short').click();
+    });
+    window.platform.icon().menu.addMenuItem('long',"Long Break", function(){
+      $('#long').click();
+    });
   }
 });
 
 function modalAlert(description){
-  if(window.fluid){
-    window.fluid.showGrowlNotification({
-      title: "Ding!",
-      description: description,
-      priority: 2,
-      sticky: true,
-      identifier: "ding",
-      icon: 'http://tomatoi.st/imgs/tomato_tiny.png'
-    });
-  }
+  notifications.notify({
+    title: "Ding!",
+    description: description,
+    sticky: true,
+    identifier: "ding",
+    icon: 'http://tomatoi.st/imgs/tomato_tiny.png'
+  });
 }
