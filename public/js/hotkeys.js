47e5708026c9a85fe4b2ac4b976d23fbe12867d6
LeviCole <lckennedy@gmail.com>
2010-01-15 09:47:08 -0500

    hotkey to show list of hotkeys

--- public/js/hotkeys.js
+++ public/js/hotkeys.js
@@ -4,16 +4,16 @@ var pKey = 80;
 var sKey = 83;
 var lKey = 76;
 
-$(document).keyup(function(e){
-  if(e.which == ctrlKey) isCtrl = false;
-});
-
-$(document).keydown(function(e){
-  // console.log(e.type,"keyCode:"+e.keyCode,"which:"+e.which,"charCode:"+String.fromCharCode(e.which));
-  if(e.which == ctrlKey) isCtrl = true;
-  if(isCtrl){
-    if($.inArray(e.keyCode, [pKey,sKey,lKey]) > -1){
-      $('#' + String.fromCharCode(e.which).toLowerCase()).submit();
+var event_type = $.browser.mozilla? "keypress" : "keydown";
+$(function () {
+  $(document).bind(event_type, function(e){
+    console.log(e.type,"keyCode:"+e.keyCode,"which:"+e.which,"charCode:"+String.fromCharCode(e.which));
+    if(e.ctrlKey){
+      if($.inArray(e.which, [pKey,sKey,lKey]) > -1){
+        $('#' + String.fromCharCode(e.which).toLowerCase()).submit();
+      }
     }
-  }
-});
+    if(e.which == 63 || e.which == 191) {$("#help").toggle()}
+  });
+  $("#close-help a.close").click(function() {$("#help").hide();})
+})
