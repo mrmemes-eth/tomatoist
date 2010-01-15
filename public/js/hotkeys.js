831211f6461f2c935a79eb817d2d82b456beed3f
LeviCole <lckennedy@gmail.com>
2010-01-15 16:02:54 -0500

    Help no longer shows up with / key

--- public/js/hotkeys.js
+++ public/js/hotkeys.js
@@ -1,5 +1,3 @@
-var isCtrl = false;
-var ctrlKey = 17;
 var pKey = 80;
 var sKey = 83;
 var lKey = 76;
@@ -7,13 +5,15 @@ var lKey = 76;
 var event_type = $.browser.mozilla? "keypress" : "keydown";
 $(function () {
   $(document).bind(event_type, function(e){
-    console.log(e.type,"keyCode:"+e.keyCode,"which:"+e.which,"charCode:"+String.fromCharCode(e.which));
+    //console.log(e.type,"keyCode:"+e.keyCode,"which:"+e.which,"charCode:"+String.fromCharCode(e.which));
     if(e.ctrlKey){
       if($.inArray(e.which, [pKey,sKey,lKey]) > -1){
         $('#' + String.fromCharCode(e.which).toLowerCase()).submit();
       }
     }
-    if(e.which == 63 || e.which == 191) {$("#help").toggle()}
+    if(e.shiftKey) {
+      if(e.which == 63 || e.which == 191) {$("#help").toggle()}
+    }
   });
   $("#close-help a.close").click(function() {$("#help").hide();})
 })
